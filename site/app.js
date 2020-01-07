7208b75198bb9f062924c16cfce5ba9bd3eeb417
Praveen Anumasu <52188926+PraveenAnumasu@users.noreply.github.com>
2020-01-07 11:28:00 +0530

    updated user data

--- site/app.js
+++ site/app.js
@@ -52,15 +52,15 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
-        "age": 38,
-        "gender": "male",
-        "street": "34 Main Street",
-        "city": "Toronto",
-        "zipcode": "M5H 1T1"
+        "name": "Rohith P",
+        "age": 45,
+        "gender": "Male",
+        "street": "19 Main, HBR Layout",
+        "city": "Bangalore",
+        "zipcode": "560043"
       },
       "medications": ["Metoprolol", "ACE inhibitors", "Vitamin D"],
-      "appointments": ["2018-01-15 1:00 - Dentist", "2018-02-14 4:00 - Internal Medicine", "2018-09-30 8:00 - Pediatry"]
+      "appointments": ["2019-01-15 1:00 - Dentist", "2019-08-14 4:00 - Internal Medicine", "2019-12-30 8:00 - Pediatry"]
     }
 
     res.send(patientdata);
